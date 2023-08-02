86988107a25cdf2d393ae589736ebf58622366a3
SamirZafar <Chaudharysamirzafar@gmail.com>
2023-08-02 18:00:29 +0100

    'Fixed Imports'

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,8 @@
 import Head from "next/head";
 import { Box } from "@chakra-ui/react";
-import Form from "@/Components/IndexPage/form";
-import Highlights from "@/Components/IndexPage/highlights";
-import HowItWorks from "@/Components/IndexPage/howItWorks";
+import Form from "../Components/IndexPage/form";
+import Highlights from "../Components/IndexPage/highlights";
+import HowItWorks from "../Components/IndexPage/howItWorks";
 
 export default function Home() {
   return (
