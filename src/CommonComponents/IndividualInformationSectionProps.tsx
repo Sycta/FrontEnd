c7672559031690eacf363dc7d47b127dc2356ac4
SamirZafar <Chaudharysamirzafar@gmail.com>
2023-08-02 18:02:05 +0100

    'Cleared git cache'

---
+++ src/CommonComponents/IndividualInformationSectionProps.tsx
@@ -0,0 +1,41 @@
+import { Flex, Text, Heading, Icon } from "@chakra-ui/react";
+import { IconType } from "react-icons";
+
+interface IndividualInformationSectionProps {
+  icon?: IconType;
+  heading: string;
+  text: string;
+}
+
+const IndividualInformationSection: React.FC<
+  IndividualInformationSectionProps
+> = ({ icon, heading, text }) => {
+  return (
+    <Flex
+      width={{ base: "100%", sm: "100%", md: "30%", lg: "20%" }}
+      justifyContent={"center"}
+      px={{ base: 4, sm: 4, md: 4, lg: 6 }}
+      py={{ base: 2, sm: 2, md: 6, lg: 6 }}
+      direction={"column"}
+      textAlign={"center"}
+      alignItems={"center"}
+      gap={2}
+    >
+      {icon && <Icon as={icon} boxSize={10} color={"#1f2e5a"} />}
+      <Heading
+        color={"#00adef"}
+        fontFamily="'M PLUS Rounded 1c', sans-serif"
+        as="h4"
+        fontSize={{ base: "16px", sm: "16px", md: "16px", lg: "20px" }}
+        fontWeight={800}
+      >
+        {heading}
+      </Heading>
+      <Text fontSize={{ base: "14px", sm: "14px", md: "14px", lg: "16px" }}>
+        {text}
+      </Text>
+    </Flex>
+  );
+};
+
+export default IndividualInformationSection;
---
+++ src/Components/ValuationPage/carDetails.tsx
@@ -0,0 +1,53 @@
+import { Flex, Box } from "@chakra-ui/react";
+import DetailsLabel from "./DetailLabel";
+
+export default function CarDetails({
+  parsedValuationData,
+  carNumberPlate,
+  carMileage,
+}: {
+  parsedValuationData: any;
+  carNumberPlate: any;
+  carMileage: any;
+}) {
+  return (
+    <Box
+      bg={"white"}
+      px={4}
+      py={4}
+      borderRight={"2px solid #EAEAEA"}
+      borderBottom={{
+        base: "2px solid #EAEAEA",
+        sm: "2px solid #EAEAEA",
+      }}
+      borderBottomLeftRadius={{ base: 0, sm: 0, md: 8, lg: 8 }}
+      borderTopRightRadius={{ base: 8, sm: 8, md: 0, lg: 0 }}
+      borderTopLeftRadius={{ base: 8, sm: 8, md: 8, lg: 8 }}
+      w={"100%"}
+    >
+      <Flex direction={"column"} textAlign={"left"} h={"100%"} px={2} py={2}>
+        <DetailsLabel
+          heading="Manufacturer"
+          text={parsedValuationData.manufacturer}
+        />
+        <DetailsLabel heading="Model" text={parsedValuationData.model} />
+        <DetailsLabel heading="Year" text={parsedValuationData.year} />
+        <DetailsLabel heading="Colour" text={parsedValuationData.colour} />
+        <DetailsLabel
+          heading="Transmission"
+          text={parsedValuationData.transmission}
+        />
+        <DetailsLabel
+          heading="Engine Size"
+          text={parsedValuationData.engineSize}
+        />
+        <DetailsLabel
+          heading="First Registered"
+          text={parsedValuationData.firstRegistered}
+        />
+        <DetailsLabel heading="Car Reg" text={carNumberPlate} />
+        <DetailsLabel heading="Car Mileage" text={carMileage} />
+      </Flex>
+    </Box>
+  );
+}
