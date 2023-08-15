ad2bb5971aeb551a28d30655d0dbe0eb17043948
Sycta <142250613+Sycta@users.noreply.github.com>
2023-08-15 20:24:47 +0100

    Update contact.tsx
    Updated Endpoint

--- src/pages/contact.tsx
+++ src/pages/contact.tsx
@@ -167,7 +167,7 @@ const ContactUsForm = () => {
       if (process.env.NODE_ENV === "development") {
         baseUrl = "http://localhost:8080";
       } else {
-        baseUrl = "https://buy-any-car-693543259e40.herokuapp.com";
+        baseUrl = "https://sycta-fab9d2536a62.herokuapp.com";
       }
 
       const url = `${baseUrl}/api/v1/email`;
